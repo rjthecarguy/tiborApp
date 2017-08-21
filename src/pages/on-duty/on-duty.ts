b64db1fba39ae57195f753bce78c0ab1be254411
rjthecarguy <rj@rjrobinson.net>
2017-08-20 22:26:46 -0700

    On Duty Page Added 2

--- src/pages/on-duty/on-duty.ts
+++ src/pages/on-duty/on-duty.ts
@@ -15,6 +15,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OnDutyPage {
 
+	position: any = "guard";
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
