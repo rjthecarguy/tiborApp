679f238d9216d012081cafe35de498d7fb1c30e4
rjthecarguy <rj@rjrobinson.net>
2017-10-25 10:56:21 -0700

    json change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,18 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
+  pages: Array<{title: string, component: any}>;
+
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+
+       this.pages = [
+      { title: 'Home', component: HomePage }
+      
+    ];
       statusBar.styleDefault();
       splashScreen.hide();
     });
